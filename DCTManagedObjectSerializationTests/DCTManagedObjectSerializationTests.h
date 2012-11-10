544342c8de6a670b37a9932bd31450da0f768acd
Daniel Tull <dt@danieltull.co.uk>
2012-11-10 17:27:47 +0000

    Add tests

---
+++ DCTManagedObjectSerializationTests/DCTManagedObjectSerializationTests.h
@@ -0,0 +1,13 @@
+//
+//  DCTManagedObjectSerializationTests.h
+//  DCTManagedObjectSerializationTests
+//
+//  Created by Daniel Tull on 10.11.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DCTManagedObjectSerializationTests : SenTestCase
+
+@end
---
+++ DCTManagedObjectSerializationTests/DCTManagedObjectSerializationTests.m
@@ -0,0 +1,32 @@
+//
+//  DCTManagedObjectSerializationTests.m
+//  DCTManagedObjectSerializationTests
+//
+//  Created by Daniel Tull on 10.11.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import "DCTManagedObjectSerializationTests.h"
+
+@implementation DCTManagedObjectSerializationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DCTManagedObjectSerializationTests");
+}
+
+@end
