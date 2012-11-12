3f2cc3b44b33b9adc608fb98e01280d71d8a28a2
Mike Abdullah <me@mikeabdullah.net>
2012-11-12 14:55:33 +0000

    Rely on -dct_shouldDeserializeNilValues: to stop nil values entering this method instead.

--- DCTManagedObjectSerialization/NSManagedObject+DCTManagedObjectSerialization.m
+++ DCTManagedObjectSerialization/NSManagedObject+DCTManagedObjectSerialization.m
@@ -14,7 +14,7 @@
 - (void)dct_setSerializedValue:(id)value forKey:(NSString *)key {
 	NSPropertyDescription *property = [self.entity.propertiesByName objectForKey:key];
 	id transformedValue = [property dct_valueForSerializedValue:value inManagedObjectContext:self.managedObjectContext];
-	if (transformedValue) [self setValue:transformedValue forKey:key];
+	[self setValue:transformedValue forKey:key];
 }
 
 @end
