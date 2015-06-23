34087af071a9bd84e22267dc52b6cd1c1582388a
Daniel Tull <dt@danieltull.co.uk>
2015-06-23 20:17:57 +0100

    Add tests

--- DCTManagedObjectSerialization Tests/DeserializerTests.swift
+++ DCTManagedObjectSerialization Tests/DeserializerTests.swift
@@ -15,6 +15,7 @@ class DeserializerTests: XCTestCase {
 
 	var managedObjectContext: NSManagedObjectContext!
 	var personEntity: NSEntityDescription!
+	var eventEntity: NSEntityDescription!
 	var personID: NSAttributeDescription {
 		return personEntity.attributesByName[PersonAttributes.personID as String]!
 	}
@@ -35,6 +36,7 @@ class DeserializerTests: XCTestCase {
 			managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
 			managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
 			personEntity = Person.entityInManagedObjectContext(managedObjectContext)
+			eventEntity = Event.entityInManagedObjectContext(managedObjectContext)
 		} catch {
 			XCTFail()
 		}
@@ -313,4 +315,76 @@ class DeserializerTests: XCTestCase {
 			XCTAssertNil(error)
 		}
 	}
+
+	func testRelationship2() {
+		let expectation = self.expectationWithDescription("testRelationship2")
+		let deserializer = Deserializer(managedObjectContext: managedObjectContext)
+		let dictionary = [ EventAttributes.name as String : "Party!", EventRelationships.person as String : [ PersonAttributes.personID as String : "1" ] ]
+		deserializer.deserializeObjectWithEntity(eventEntity, dictionary: dictionary) { object in
+			defer {	expectation.fulfill() }
+			guard let event = object as? Event else {
+				XCTFail()
+				return
+			}
+			XCTAssertEqual(event.name, "Party!")
+			guard let person = event.person else {
+				XCTFail()
+				return
+			}
+			XCTAssertEqual(person.events.count, 1)
+			guard let personID = person.personID else {
+				XCTFail()
+				return
+			}
+			XCTAssertEqual(personID, "1")
+		}
+		waitForExpectationsWithTimeout(30) { error in
+			XCTAssertNil(error)
+		}
+	}
+
+	func testRelationshipDuplicate() {
+		let expectation = self.expectationWithDescription("testRelationship2")
+		var serializationInfo = SerializationInfo()
+		serializationInfo.uniqueAttributes[personEntity] = [personID]
+		let deserializer = Deserializer(managedObjectContext: managedObjectContext, serializationInfo: serializationInfo)
+		let personDictionary = [ PersonAttributes.personID as String : "1" ]
+		let event1Dictionary: SerializedDictionary = [ EventAttributes.name as String : "Party 1!", EventRelationships.person as String : personDictionary ]
+		let event2Dictionary: SerializedDictionary = [ EventAttributes.name as String : "Party 2!", EventRelationships.person as String : personDictionary ]
+		let array = [ event1Dictionary, event2Dictionary ]
+		deserializer.deserializeObjectsWithEntity(eventEntity, array: array) { objects in
+
+			defer {	expectation.fulfill() }
+
+			XCTAssertEqual(objects.count, 2)
+
+			guard let events = objects as? [Event] else {
+				XCTFail()
+				return
+			}
+			let event1 = events[0]
+			let event2 = events[1]
+//			XCTAssertEqual(event1.name, "Party 1!")
+//			XCTAssertEqual(event2.name, "Party 2!")
+			guard let person1 = event1.person else {
+				XCTFail()
+				return
+			}
+			guard let person2 = event2.person else {
+				XCTFail()
+				return
+			}
+			XCTAssertEqual(person1.personID, "1")
+			XCTAssertEqual(person2.personID, "1")
+			XCTAssertEqual(person1, person2)
+		}
+
+		waitForExpectationsWithTimeout(30) { error in
+			XCTAssertNil(error)
+		}
+	}
+
+
+
+
 }
