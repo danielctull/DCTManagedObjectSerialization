2b2a5f9cff9143fb1b365e512a1433a49cb96758
Daniel Tull <dt@danieltull.co.uk>
2016-03-04 18:55:04 +0000

    Rename to Deserializer

--- DCTManagedObjectSerialization/DCTManagedObjectSerialization.h
+++
@@ -1,5 +0,0 @@
-
-@import Foundation;
-
-FOUNDATION_EXPORT double DCTManagedObjectSerializationVersionNumber;
-FOUNDATION_EXPORT const unsigned char DCTManagedObjectSerializationVersionString[];
---
+++ Deserializer/Deserializer.h
@@ -0,0 +1,5 @@
+
+@import Foundation;
+
+FOUNDATION_EXPORT double DeserializerVersionNumber;
+FOUNDATION_EXPORT const unsigned char DeserializerVersionString[];
